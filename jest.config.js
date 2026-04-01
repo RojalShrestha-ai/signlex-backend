8378b496ff21fd620185c85bb3e87b85f57b8ea1
aminmemonn <aminmemonict@gmail.com>
2026-04-01 19:22:00 +0545

    add test setup with jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js"],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/server.js",
+    "!src/config/**",
+  ],
+  coverageDirectory: "coverage",
+  verbose: true,
+  testTimeout: 10000,
+};
